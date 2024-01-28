301fad3c68ce5d585c92dabab0bf0abed1873d2f
zoltan <vaszilyzoltan28@gmail.com>
2024-01-28 09:39:45 +0100

    project init

---
+++ src/main/java/com/example/SkillTribe/SkillTribeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkillTribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkillTribe/SkillTribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkillTribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
