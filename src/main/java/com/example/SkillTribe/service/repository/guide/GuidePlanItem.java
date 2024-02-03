9c16feaee31de34769d40c1e78df795a4b2a6d97
zoltan <vaszilyzoltan28@gmail.com>
2024-02-03 21:05:54 +0100

    GuidePlanRepository named as GuidePlan, fix to correct name

--- src/main/java/com/example/SkillTribe/service/repository/guide/GuidePlanItem.java
+++
@@ -1,6 +0,0 @@
-package com.example.SkillTribe.service.repository.guide;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuidePlanItem extends JpaRepository<GuidePlanItem, Long> {
-}
